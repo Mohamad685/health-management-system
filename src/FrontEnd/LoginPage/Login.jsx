5e14b35309b6d9413102b8b75b9afc1e9cb38941
Mohammad Fakih <Mohammad.fakih685@gmail.com>
2023-12-05 03:31:29 +0200

    sign in added with its api

--- src/FrontEnd/LoginPage/Login.jsx
+++ src/FrontEnd/LoginPage/Login.jsx
@@ -1,18 +1,33 @@
-// Login.js
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom'; // React Router's useHistory hook
+import axios from 'axios'; // Import Axios for making HTTP requests
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  // const history = useHistory();
+  const navigate = useNavigate(); // Get the navigate function from React Router
 
   const handleLogin = async () => {
-    // Implement authentication logic here using Axios or Fetch
-    // If authentication is successful, redirect to the respective user page
-    // For simplicity, assuming a successful login redirects to the Admin page
-    history.push('/admin');
+    try {
+      // Make a request to the sign-in API
+      const response = await axios.post('/api/admin/signin', {
+        username,
+        password,
+      });
+
+      if (response.data.success) {
+        // Authentication successful, navigate to the admin page
+        navigate('/admin');
+      } else {
+        // Authentication failed, handle accordingly (e.g., show an error message)
+        console.error('Invalid username or password');
+      }
+    } catch (error) {
+      // Handle any error that occurs during the API request
+      console.error('Error during login:', error);
+    }
   };
+
 
   return (
     <div>
@@ -40,8 +55,8 @@ const Login = () => {
           </label>
           <br />
           <button type="button" onClick={handleLogin}>
-            Login
-          </button>
+          Login
+        </button>
         </form>
       </main>
       <footer>
